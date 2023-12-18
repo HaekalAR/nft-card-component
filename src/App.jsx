3dbce10c822c73df129cff2eccc341b281d5844e
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2023-12-18 19:46:49 +0700

    Adding color to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from "./Component/Card";
 
 const App = () => {
   return (
-    <div className="md:px-0 px-2 bg-r bg-r-darkerBlueMainAdd">
+    <div className="md:px-0 px-2 bg-r bg-r-darkerBlueMain">
       <Card />
     </div>
   );
