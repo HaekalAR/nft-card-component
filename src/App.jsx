76670d54802eda9284c0850c91a218a611f9111c
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2023-12-18 19:46:31 +0700

    Adding background color to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Card from "./Component/Card";
 
 const App = () => {
   return (
-    <div className="md:px-0 px-2 bg-[--darker-blue-main]">
+    <div className="md:px-0 px-2 bg-r bg-r-darkerBlueMainAdd">
       <Card />
     </div>
-  )
-}
+  );
+};
 
 export default App;
